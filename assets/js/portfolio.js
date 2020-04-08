cc0560713d6c96c3eccf704dc9927694effd5b86
Joao Victor <j.victor.dantas@hotmail.com>
2020-04-07 20:32:36 -0500

    info added

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -23,12 +23,14 @@ $(window).scroll(function () {
   }
 });
 
+let titles = ["Pomodoro App", "Soccer Trivia Game", "Github Repos Ranking"];
+
 let imgs = ["./assets/img/1.gif", "./assets/img/2.gif", "./assets/img/3.gif"];
 
 let descs = [
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas aliquid excepturi quia commodi saepe dolores fugiat iusto enim vero repudiandae corporis, consequuntur doloribus veritatis. Ex.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas aliquid excepturi quia commodi saepe dolores fugiat iusto enim vero repudiandae corporis, consequuntur doloribus veritatis. Ex.",
-  "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas aliquid excepturi quia commodi saepe dolores fugiat iusto enim vero repudiandae corporis, consequuntur doloribus veritatis. Ex.",
+  "Pomodoro App with expected functionalities. These involve setting a work and break time, along with a set ratio between them, to make setting longer intervals easier. Very useful for effective working.",
+  "Soccer Trivia Game inspired on mobile game Trivia Crack. Main functionalities work perfectly, but is still a work in progress without a menu. Since the game was made with a Brazillian audience in mind, it is in Portuguese (right/wrong labels for questions are enabled for English Speakers to test the functionality). An API for the game is also currently in development.",
+  "App that gathers info from Github's API to rank repositories according to number of stars. Rankings displayed are for repos created last week, month, year and all time.",
 ];
 
 let links1 = [
@@ -43,26 +45,52 @@ let links2 = [
   "https://github.com/JVRD270/github-repo-ranking",
 ];
 
-function Project(img, description, link1, link2) {
+function Project(title, img, description, link1, link2) {
+  this.title = title;
   this.img = img;
   this.description = description;
   this.link1 = link1;
   this.link2 = link2;
 }
 
-let CurrentProject = new Project(imgs[0], descs[0], links1[0], links2[0]);
+let CurrentProject = new Project(
+  titles[0],
+  imgs[0],
+  descs[0],
+  links1[0],
+  links2[0]
+);
 
 projects.on("click", function () {
   if ($(this).hasClass("proj1")) {
-    CurrentProject = new Project(imgs[0], descs[0], links1[0], links2[0]);
+    CurrentProject = new Project(
+      titles[0],
+      imgs[0],
+      descs[0],
+      links1[0],
+      links2[0]
+    );
   } else if ($(this).hasClass("proj2")) {
-    CurrentProject = new Project(imgs[1], descs[1], links1[1], links2[1]);
+    CurrentProject = new Project(
+      titles[1],
+      imgs[1],
+      descs[1],
+      links1[1],
+      links2[1]
+    );
   } else if ($(this).hasClass("proj3")) {
-    CurrentProject = new Project(imgs[2], descs[2], links1[2], links2[2]);
+    CurrentProject = new Project(
+      titles[2],
+      imgs[2],
+      descs[2],
+      links1[2],
+      links2[2]
+    );
   }
   card.addClass("slideIn");
   card.removeClass("slideOut");
   $(".overlay").addClass("darken");
+  $(".popup h3").text(CurrentProject.title);
   $(".popup .projectImg").attr("src", CurrentProject.img);
   $(".popup p").text(CurrentProject.description);
   $(".popup .btn1").attr("href", CurrentProject.link1);
